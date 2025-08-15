cdedcb3ec7d641871a252ac73941192cd9ef5f3f
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-08-15 13:11:50 +0300

    Exceptions global

---
+++ 1.Task-ProductApi/EntityLayer/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace _1.Task_ProductApi.EntityLayer.Exceptions
+{
+    public class NotFoundException:Exception
+    {
+        public NotFoundException(string message): base(message)
+        {
+        }
+    }
+}
