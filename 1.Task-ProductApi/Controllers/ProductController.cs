22dbbf2bd7d8fd0f8b79cb18e57f1fb41da5409b
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-08-15 13:11:05 +0300

    ProductController

--- 1.Task-ProductApi/Controllers/ProductController.cs
+++ 1.Task-ProductApi/Controllers/ProductController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _1.Task_ProductApi.BusinessLayer.Abstract;
+using _1.Task_ProductApi.BusinessLayer.Concrete;
+using _1.Task_ProductApi.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _1.Task_ProductApi.Controllers
 {
-    public class ProductController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            return View();
+            _productService = productService;
         }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> CreateProduct(CreateProductDto createProductDto)
+        {
+            var product = await _productService.CreateProductAsync(createProductDto);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+         
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+
+
     }
 }
