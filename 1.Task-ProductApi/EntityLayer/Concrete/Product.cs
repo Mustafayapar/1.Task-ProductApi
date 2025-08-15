1e9b60cbff284eb2897887a90a09a31a102dc5d5
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-08-15 09:53:48 +0300

    Product Entity

---
+++ 1.Task-ProductApi/EntityLayer/Concrete/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace _1.Task_ProductApi.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; } // Stok adedi
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    }
+}
